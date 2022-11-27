b26e70bca62ee0ef110e44d9075820a4b5b65c48
Dimitris Spachos <dimitris.spachos@amazeelabs.com>
2022-11-27 08:01:00 +0200

    Update 2022-11-27 08:01:00

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 wget -O all.json https://www.med.auth.gr/feeds/json/all
 dt="`date "+%Y-%m-%d %H:%M:%S"`";
-git add . && git commit -m"Update $Dt" && git push
+git add . && git commit -m"Update $dt" && git push
 
 
 
